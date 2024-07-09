2f8b4b6bac8f0fb96cb365f49e7564ffabb81dce
irmaktekin <irmaktekin97@gmail.com>
2024-07-09 22:33:50 +0300

    Project configuration is done.

---
+++ src/main/java/com/patika/Main.java
@@ -0,0 +1,14 @@
+package com.patika;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("library");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+    }
+
+}
