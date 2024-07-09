e8f34b6d82979f5c9c61d19932e8548ef142e27a
irmaktekin <irmaktekin97@gmail.com>
2024-07-09 23:27:18 +0300

    BookBorrowing entity is created.

---
+++ src/main/java/com/patika/BookBorrowing.java
@@ -0,0 +1,59 @@
+package com.patika;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "book_borrowing")
+public class BookBorrowing {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
+    private int id;
+
+    @Column(name = "borrower_name",length = 100, nullable = false)
+    private String borrowerName;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "borrowing_date", nullable = false)
+    private java.util.Date borrowingDate;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "return_date")
+    private java.util.Date returnDate;
+
+    public Date getBorrowingDate() {
+        return borrowingDate;
+    }
+
+    public void setBorrowingDate(Date borrowingDate) {
+        this.borrowingDate = borrowingDate;
+    }
+
+    public Date getReturnDate() {
+        return returnDate;
+    }
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
+    }
+
+    public String getBorrowerName() {
+        return borrowerName;
+    }
+
+    public void setBorrowerName(String borrowerName) {
+        this.borrowerName = borrowerName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public BookBorrowing() {
+    }
+}
