d57fe738a896b271487e1d7247307254f21dd577
irmaktekin <irmaktekin97@gmail.com>
2024-07-09 23:24:24 +0300

    Publisher entity is created.

---
+++ src/main/java/com/patika/Publisher.java
@@ -0,0 +1,60 @@
+package com.patika;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "publisher")
+public class Publisher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "publisher_id")
+    private int id;
+
+    @Column(name = "publisher_name",length = 100, nullable = false)
+    private String publisherName;
+
+    @Temporal(TemporalType.DATE)
+    @Column(name = "establish_year", nullable = false)
+    private java.util.Date establishYear;
+
+    @Column(name = "publisher_address",length = 200, nullable = false)
+    private String publisherAddress;
+
+    public Date getEstablishYear() {
+        return establishYear;
+    }
+
+    public void setEstablishYear(Date establishYear) {
+        this.establishYear = establishYear;
+    }
+
+    public String getPublisherAddress() {
+        return publisherAddress;
+    }
+
+    public void setPublisherAddress(String publisherAddress) {
+        this.publisherAddress = publisherAddress;
+    }
+
+    public String getPublisherName() {
+        return publisherName;
+    }
+
+    public void setPublisherName(String publisherName) {
+        this.publisherName = publisherName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Publisher() {
+    }
+
+}
