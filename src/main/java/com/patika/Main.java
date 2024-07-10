38312b13035217fa0b89e937d84f685e2345b742
irmaktekin <irmaktekin97@gmail.com>
2024-07-10 23:35:39 +0300

    Example datas are inserted.

--- src/main/java/com/patika/Main.java
+++ src/main/java/com/patika/Main.java
@@ -1,14 +1,29 @@
 package com.patika;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
+import javax.persistence.*;
+import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
 
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("library");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
-    }
+        EntityTransaction entityTransaction = entityManager.getTransaction();
 
+       /* Author author = new Author();
+        author.setBirthDate(new Date(20,8,5));
+        author.setName("Authoer1");
+        author.setCountry("Turkey");
+        entityManager.persist(author);
+
+        entityTransaction.begin();
+        Book book1 = new Book();
+        book1.setName("Book1");
+        book1.setBook_stock(20);
+        book1.setPublicationYear(2023);
+        book1.setAuthor(author);
+        entityManager.persist(book1);
+        */
+
+    }
 }
